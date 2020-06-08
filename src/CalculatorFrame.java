cbb226e1ad16afdd379fdc64f1eaf76d4151776f
Iman Mousaei <imanmousaei1379@gmail.com>
2020-06-08 20:12:49 +0430

    cleaned

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -8,80 +8,72 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class CalculatorFrame extends JFrame
-{  
-   private JTextField display;
+public class CalculatorFrame extends JFrame {
+    private JTextField display;
 
-   private static final int FRAME_WIDTH = 300;
-   private static final int FRAME_HEIGHT = 300;
+    private static final int FRAME_WIDTH = 300;
+    private static final int FRAME_HEIGHT = 300;
 
-   public CalculatorFrame()
-   {
-      display = new JTextField("");
-      display.setEditable(false);           
-      add(display, BorderLayout.NORTH);
-      createButtonPanel();
-      setSize(FRAME_WIDTH, FRAME_HEIGHT);
-   }
-      
-   /**
-      Creates the button panel.
-   */
-   private void createButtonPanel()
-   {      
-      JPanel buttonPanel = new JPanel();
-      buttonPanel.setLayout(new GridLayout(4, 4));
+    public CalculatorFrame() {
+        display = new JTextField("");
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+        createButtonPanel();
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    }
 
-      buttonPanel.add(makeDigitButton("7"));
-      buttonPanel.add(makeDigitButton("8"));
-      buttonPanel.add(makeDigitButton("9"));
-      buttonPanel.add(makeOperatorButton("/"));
-      buttonPanel.add(makeDigitButton("4"));
-      buttonPanel.add(makeDigitButton("5"));
-      buttonPanel.add(makeDigitButton("6"));
-      buttonPanel.add(makeOperatorButton("*"));
-      buttonPanel.add(makeDigitButton("1"));
-      buttonPanel.add(makeDigitButton("2"));
-      buttonPanel.add(makeDigitButton("3"));
-      buttonPanel.add(makeOperatorButton("-"));
-      buttonPanel.add(makeDigitButton("0"));
-      buttonPanel.add(makeDigitButton("."));
-      buttonPanel.add(makeOperatorButton("="));
-      buttonPanel.add(makeOperatorButton("+"));
+    /**
+     * Creates the button panel.
+     */
+    private void createButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
 
-      add(buttonPanel, BorderLayout.CENTER);
-   }
-   
-   class DigitButtonListener implements ActionListener
-   {
-      private String digit;
-      
+        buttonPanel.add(makeDigitButton("7"));
+        buttonPanel.add(makeDigitButton("8"));
+        buttonPanel.add(makeDigitButton("9"));
+        buttonPanel.add(makeOperatorButton("/"));
+        buttonPanel.add(makeDigitButton("4"));
+        buttonPanel.add(makeDigitButton("5"));
+        buttonPanel.add(makeDigitButton("6"));
+        buttonPanel.add(makeOperatorButton("*"));
+        buttonPanel.add(makeDigitButton("1"));
+        buttonPanel.add(makeDigitButton("2"));
+        buttonPanel.add(makeDigitButton("3"));
+        buttonPanel.add(makeOperatorButton("-"));
+        buttonPanel.add(makeDigitButton("0"));
+        buttonPanel.add(makeDigitButton("."));
+        buttonPanel.add(makeOperatorButton("="));
+        buttonPanel.add(makeOperatorButton("+"));
 
-      public DigitButtonListener(String aDigit)
-      {
-         digit = aDigit;
-      }
-      
-      public void actionPerformed(ActionEvent event)
-      {  
-         display.setText(display.getText() + digit);
-      }        
-   }
+        add(buttonPanel, BorderLayout.CENTER);
+    }
+
+    class DigitButtonListener implements ActionListener {
+        private String digit;
 
 
-   public JButton makeDigitButton(String digit)
-   {
-      JButton button = new JButton(digit);
-      
-      ActionListener listener = new DigitButtonListener(digit);
-      button.addActionListener(listener);  
-      return button;    
-   } 
+        public DigitButtonListener(String aDigit) {
+            digit = aDigit;
+        }
+
+        public void actionPerformed(ActionEvent event) {
+            display.setText(display.getText() + digit);
+        }
+    }
 
 
-   public JButton makeOperatorButton(String op)
-   {
-      JButton button = new JButton(op);
-      return button;    
-   }     
+    public JButton makeDigitButton(String digit) {
+        JButton button = new JButton(digit);
+
+        ActionListener listener = new DigitButtonListener(digit);
+        button.addActionListener(listener);
+        return button;
+    }
+
+
+    public JButton makeOperatorButton(String op) {
+        JButton button = new JButton(op);
+        return button;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import javax.swing.JFrame;
 
-public class Main
-{  
-   public static void main(String[] args)
-   {  
-      JFrame frame = new CalculatorFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setTitle("Calculator");
-      frame.setVisible(true);
-   }
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new CalculatorFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Calculator");
+        frame.setVisible(true);
+    }
 }
